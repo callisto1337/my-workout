5f850e013816d177d0fa970b4d8a1d4d7453c9a4
callisto1337 <hazardous333@gmail.com>
2022-12-06 23:54:42 +0300

    added configs etc

--- configs/webpack/plugins/index.mjs
+++ configs/webpack/plugins/index.mjs
@@ -1,9 +1,11 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import DotEnv from 'dotenv-webpack';
 
 export default [
   new HtmlWebpackPlugin({
     template: './index.html',
   }),
   new ForkTsCheckerWebpackPlugin(),
+  new DotEnv(),
 ];
